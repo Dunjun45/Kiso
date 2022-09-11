3f39221e2e63e9a83a3f941f34022c6f2d3a1d57
dunjun45 <dunjun45@gmail.com>
2022-09-11 23:55:51 +0800

    added premake build

---
+++ premake5.lua
@@ -0,0 +1,96 @@
+workspace "Kiso"
+	architecture "x64"
+
+	configurations {
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Kiso"
+	location "Kiso"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.22000.0"
+
+		defines {
+			"KS_PLATFORM_WINDOWS",
+			"KS_BUILD_DLL"
+		}
+
+		postbuildcommands {
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "KS_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "KS_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "KS_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"Kiso/vendor/spdlog/include",
+		"Kiso/src"
+	}
+
+	links {
+		"Kiso"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.22000.0"
+
+		defines {
+			"KS_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "KS_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "KS_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "KS_DIST"
+		optimize "On"
